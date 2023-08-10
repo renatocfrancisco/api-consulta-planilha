c5f3108447dff49d24b4dc317052a9d0eed93067
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-08-10 09:50:14 -0300

    style: :art: formatar código python (black)

--- filter.py
+++ filter.py
@@ -9,76 +9,75 @@ def filterSpreadsheet(data):
     def filtrarDataframe(data, df):
         def idade(df, desde, ate):
             if desde and ate:
-                df = df[(df['idade'] >= desde) & (df['idade'] <= ate)]
+                df = df[(df["idade"] >= desde) & (df["idade"] <= ate)]
             elif desde:
-                df = df[df['idade'] >= desde]
+                df = df[df["idade"] >= desde]
             elif ate:
-                df = df[df['idade'] <= ate]
+                df = df[df["idade"] <= ate]
             return df
 
         def parcela(df, desde, ate):
             if desde and ate:
-                df = df[(df['parcela'] >= desde) & (df['parcela'] <= ate)]
+                df = df[(df["parcela"] >= desde) & (df["parcela"] <= ate)]
             elif desde:
-                df = df[df['parcela'] >= desde]
+                df = df[df["parcela"] >= desde]
             elif ate:
-                df = df[df['parcela'] <= ate]
+                df = df[df["parcela"] <= ate]
             return df
 
         def soma_parcela(df, desde, ate):
             if desde and ate:
-                df = df[(df['soma parcela'] >= desde)
-                        & (df['soma parcela'] <= ate)]
+                df = df[(df["soma parcela"] >= desde) & (df["soma parcela"] <= ate)]
             elif desde:
-                df = df[df['soma parcela'] >= desde]
+                df = df[df["soma parcela"] >= desde]
             elif ate:
-                df = df[df['soma parcela'] <= ate]
+                df = df[df["soma parcela"] <= ate]
             return df
 
         def juros(df, desde, ate):
             if desde and ate:
-                df = df[(df['juros'] >= desde)
-                        & (df['juros'] <= ate)]
+                df = df[(df["juros"] >= desde) & (df["juros"] <= ate)]
             elif desde:
-                df = df[df['juros'] >= desde]
+                df = df[df["juros"] >= desde]
             elif ate:
-                df = df[df['juros'] <= ate]
+                df = df[df["juros"] <= ate]
             return df
 
         def esp(df, arrayEsp):
-            df = df[df['esp'].isin(arrayEsp)]
+            df = df[df["esp"].isin(arrayEsp)]
             return df
 
         def banco_emp(df, arrayBancos):
-            df = df[df['banco emp'].isin(arrayBancos)]
+            df = df[df["banco emp"].isin(arrayBancos)]
             return df
 
         def banco_pgto(df, arrayBancos):
-            df = df[df['banco PGTO'].isin(arrayBancos)]
+            df = df[df["banco PGTO"].isin(arrayBancos)]
             return df
 
-        df[['parcela', 'soma parcela', 'juros']] = df[['parcela', 'soma parcela',
-                                                       'juros']].apply(lambda x: x.str.replace(',', '.').astype(float))
+        df[["parcela", "soma parcela", "juros"]] = df[
+            ["parcela", "soma parcela", "juros"]
+        ].apply(lambda x: x.str.replace(",", ".").astype(float))
 
-        df = idade(df, data['idadeMin'], data['idadeMax'])
+        df = idade(df, data["idadeMin"], data["idadeMax"])
         if df.empty:
             return df
-        df = parcela(df, data['parcelaMin'], data['parcelaMax'])
+        df = parcela(df, data["parcelaMin"], data["parcelaMax"])
         if df.empty:
             return df
-        df = soma_parcela(df, data['parcelasPagasMin'], data['parcelasPagasMax'])
+        df = soma_parcela(df, data["parcelasPagasMin"], data["parcelasPagasMax"])
         if df.empty:
             return df
-        df = juros(df, data['jurosMin'], data['jurosMax'])
+        df = juros(df, data["jurosMin"], data["jurosMax"])
         if df.empty:
             return df
-        df = esp(df, data['esp'])
+        df = esp(df, data["esp"])
         if df.empty:
             return df
-        df = banco_emp(df, data['banco_emp'])
+        df = banco_emp(df, data["banco_emp"])
         if df.empty:
             return df
-        df = banco_pgto(df, data['banco_pgto'])
+        df = banco_pgto(df, data["banco_pgto"])
 
         return df
 
@@ -90,29 +89,31 @@ def filterSpreadsheet(data):
 
     arquivos_planilha = []
 
-    for uf in data['uf']:
+    for uf in data["uf"]:
         if uf in brazilian_states:
-            arquivos_planilha.append('csv/' + uf + '.csv')
+            arquivos_planilha.append("csv/" + uf + ".csv")
 
     if arquivos_planilha == []:
         return []
 
     dfs = []
     for arquivo in arquivos_planilha:
-        df = pd.read_csv(arquivo, delimiter=';',
-                         chunksize=get_optimal_chunksize(arquivo))
+        df = pd.read_csv(
+            arquivo, delimiter=";", chunksize=get_optimal_chunksize(arquivo)
+        )
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
             futures = []
 
-            futures.extend(executor.submit(filtrarDataframe, data, chunk)
-                           for chunk in df)
+            futures.extend(
+                executor.submit(filtrarDataframe, data, chunk) for chunk in df
+            )
             for future in concurrent.futures.as_completed(futures):
                 dfs.append(future.result())
 
             executor.shutdown(wait=True)
 
     dados_combinados = pd.concat(dfs, ignore_index=True)
-    dados_combinados.to_csv('planilha_combinada.csv', index=False)
+    dados_combinados.to_csv("planilha_combinada.csv", index=False)
     returnData = dados_combinados.to_csv(index=False)
 
     return returnData
--- main.py
+++ main.py
@@ -7,45 +7,99 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def get_data():
-    return jsonify({'message': 'Hello world'}), status.HTTP_200_OK
+    return jsonify({"message": "Hello world"}), status.HTTP_200_OK
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def post_data():
     data = request.get_json()
 
     if not data:
-        return (jsonify({'message': 'No data received'}), status.HTTP_400_BAD_REQUEST)
+        return (jsonify({"message": "No data received"}), status.HTTP_400_BAD_REQUEST)
 
-    for key in ('idadeMin', 'idadeMax', 'parcelaMin', 'parcelaMax', 'parcelasPagasMin', 'parcelasPagasMax', 'jurosMin', 'jurosMax'):
+    for key in (
+        "idadeMin",
+        "idadeMax",
+        "parcelaMin",
+        "parcelaMax",
+        "parcelasPagasMin",
+        "parcelasPagasMax",
+        "jurosMin",
+        "jurosMax",
+    ):
         if key not in data:
             data[key] = 0
 
-    if not all(key in data for key in ('uf', 'idadeMin', 'idadeMax', 'parcelaMin', 'parcelaMax', 'parcelasPagasMin', 'parcelasPagasMax', 'jurosMin', 'jurosMax', 'esp', 'banco_emp', 'banco_pgto')):
-        return (jsonify({'message': 'Data has missing keys'}), status.HTTP_400_BAD_REQUEST)
+    if not all(
+        key in data
+        for key in (
+            "uf",
+            "idadeMin",
+            "idadeMax",
+            "parcelaMin",
+            "parcelaMax",
+            "parcelasPagasMin",
+            "parcelasPagasMax",
+            "jurosMin",
+            "jurosMax",
+            "esp",
+            "banco_emp",
+            "banco_pgto",
+        )
+    ):
+        return (
+            jsonify({"message": "Data has missing keys"}),
+            status.HTTP_400_BAD_REQUEST,
+        )
 
-    if not all(isinstance(data[key], int) for key in ('idadeMin', 'idadeMax', 'parcelaMin', 'parcelaMax', 'parcelasPagasMin', 'parcelasPagasMax', 'jurosMin', 'jurosMax')):
-        return (jsonify({'message': 'Data has invalid values', 'data': data}), status.HTTP_400_BAD_REQUEST)
+    if not all(
+        isinstance(data[key], int)
+        for key in (
+            "idadeMin",
+            "idadeMax",
+            "parcelaMin",
+            "parcelaMax",
+            "parcelasPagasMin",
+            "parcelasPagasMax",
+            "jurosMin",
+            "jurosMax",
+        )
+    ):
+        return (
+            jsonify({"message": "Data has invalid values", "data": data}),
+            status.HTTP_400_BAD_REQUEST,
+        )
 
-    if not all(isinstance(data[key], list) for key in ('uf', 'esp', 'banco_emp', 'banco_pgto')):
-        return (jsonify({'message': 'Data has invalid values', 'data': data}), status.HTTP_400_BAD_REQUEST)
+    if not all(
+        isinstance(data[key], list) for key in ("uf", "esp", "banco_emp", "banco_pgto")
+    ):
+        return (
+            jsonify({"message": "Data has invalid values", "data": data}),
+            status.HTTP_400_BAD_REQUEST,
+        )
 
     result = filterSpreadsheet(data)
 
     if not result:
-        return (jsonify({'message': 'No data found'}), status.HTTP_404_NOT_FOUND)
+        return (jsonify({"message": "No data found"}), status.HTTP_404_NOT_FOUND)
 
     if not isinstance(result, str):
-        return (jsonify({'message': 'Internal server error'}), status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return (
+            jsonify({"message": "Internal server error"}),
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
-    return (Response(
-        result,
-        mimetype='text/csv',
-        headers={'Content-Disposition': 'attachment; filename=data.csv'}
-    ), status.HTTP_200_OK)
+    return (
+        Response(
+            result,
+            mimetype="text/csv",
+            headers={"Content-Disposition": "attachment; filename=data.csv"},
+        ),
+        status.HTTP_200_OK,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
